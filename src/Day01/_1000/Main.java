ec6a0e0b730c8490d678130d53059aaced62a565
yehyeonn <yehyeon99@naver.com>
2024-07-12 15:02:26 +0900

    # Day01 2501: A+B

---
+++ src/Day01/_1000/Main.java
@@ -0,0 +1,18 @@
+package Day01._1000;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        int answer = num1 + num2;
+        System.out.println(answer);
+
+        sc.close();
+    }
+}
