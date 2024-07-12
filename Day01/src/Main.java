24975e78c3449d199b349a248370e3adaa0a2b80
yehyeonn <yehyeon99@naver.com>
2024-07-12 12:23:36 +0900

    # Day01 2501: 약수 구하기

---
+++ Day01/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        int cnt = 0;
+        for (int i = 1; i <= num1; i++) {
+            if(num1 % i == 0) cnt++;
+            if(cnt == num2) {
+                System.out.println(i);
+                break;
+            }
+        }
+        if(cnt <  num2) System.out.println(0);
+    }
+}
