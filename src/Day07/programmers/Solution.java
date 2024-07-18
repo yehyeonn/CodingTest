f22db8b98aa2c71705b21d66c55e61bf82f2dee2
yehyeonn <yehyeon99@naver.com>
2024-07-18 13:44:53 +0900

    # Day07 프로그래머스: 공원 산책

---
+++ src/Day07/programmers/Solution.java
@@ -0,0 +1,60 @@
+package Day07.programmers;
+
+class Solution {
+    public int[] solution(String[] park, String[] routes) {
+        int x = 0, y = 0;
+        int width = park.length;
+        int height = park[0].length();
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (park[i].charAt(j) == 'S') {
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+
+        for (String route : routes) {
+            String[] r = route.split(" ");
+            String op = r[0];
+            int step = Integer.parseInt(r[1]);
+
+            int opX = 0, opY = 0;
+            switch (op) {
+                case "N":
+                    opX = -1;
+                    break;
+                case "S":
+                    opX = 1;
+                    break;
+                case "W":
+                    opY = -1;
+                    break;
+                case "E":
+                    opY = 1;
+                    break;
+            }
+
+            int moveX = x, moveY = y;
+            boolean fleg = true;
+            for(int i = 0; i < step; i++) {
+                moveX += opX;
+                moveY += opY;
+
+                if(moveX < 0 || moveX >= width || moveY < 0 || moveY >= height || park[moveX].charAt(moveY) == 'X') {
+                    fleg = false;
+                    break;
+                }
+            }
+
+            if(fleg) {
+                x = moveX;
+                y = moveY;
+            }
+        }
+
+            int[] answer = new int[]{x, y};
+            return answer;
+    }
+}
