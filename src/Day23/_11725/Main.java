e1aa4cba675dc495485ae4dfeee3f7e681f0ae06
yehyeonn <yehyeon99@naver.com>
2024-08-03 23:58:01 +0900

    # Day23 11725: 트리의 부모 찾기

--- src/Day23/_11725/Main.java
+++ src/Day23/_11725/Main.java
@@ -1,9 +1,7 @@
 package Day23._11725;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -12,12 +10,40 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
 
         ArrayList<ArrayList<Integer>> tree = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            tree.add(new ArrayList<>());
+        for (int i = 0; i < n + 1; i++) {
+            tree.add(new ArrayList<>());    // 자식 노드가 없는 정점 n + 1 개 생성
         }
 
         for (int i = 0; i <n-1; i++) {
-            // TODO
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            tree.get(a).add(b);
+            tree.get(b).add(a);
         }
+
+        int[] p = new int[n + 1];   // 각 노드의 부모 저장
+        boolean[] visited = new boolean[n + 1];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        visited[1] = true;
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            for(int nodes : tree.get(node)) {
+                if(!visited[nodes]) {
+                    visited[nodes] = true;
+                    p[nodes] = node;
+
+                    queue.add(nodes);
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 2; i <= n; i++) {
+            sb.append(p[i]).append("\n");
+        }
+        System.out.println(sb);
     }
 }
