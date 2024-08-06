5c20a5045cf527aec465010d1c670487ad009bd0
yehyeonn <yehyeon99@naver.com>
2024-08-06 23:02:05 +0900

    # Day26 2563: 색종이

---
+++ src/Day26/_2563/Main.java
@@ -0,0 +1,26 @@
+package Day26._2563;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] canvas = new int[100][100];
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k < 10; k++) {
+                    canvas[x+j][y+k]++;
+                    if(canvas[x+j][y+k] == 1) cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
