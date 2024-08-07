4484dfbd074b1cdc67de178c0e23dfedbc89bce9
yehyeonn <yehyeon99@naver.com>
2024-08-07 13:38:13 +0900

    # Day26 프로그래머스: 0 떼기

---
+++ src/Day27/programmers/Solution.java
@@ -0,0 +1,9 @@
+package Day27.programmers;
+
+class Solution {
+    public String solution(String n_str) {
+        int n = Integer.parseInt(n_str);
+        String answer = Integer.toString(n);
+        return answer;
+    }
+}
