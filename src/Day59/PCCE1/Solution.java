719177eedc7774435b4d32cf865effba120a584d
yehyeonn <yehyeon99@naver.com>
2024-09-23 17:45:06 +0900

    # Day59 프로그래머스 PCCE1~6: 문자 출력, 각도 합치기, 수 나누기, 병과분류, 심폐소생술, 물 부족

---
+++ src/Day59/PCCE1/Solution.java
@@ -0,0 +1,10 @@
+package Day59.PCCE1;
+
+public class Solution {
+    public static void main(String[] args) {
+        String message = "Let's go!";
+
+        System.out.println("3\n2\n1");
+        System.out.println(message);
+    }
+}
---
+++ src/Day59/PCCE6/Solution.java
@@ -0,0 +1,15 @@
+package Day59.PCCE6;
+
+class Solution {
+    public int solution(int storage, int usage, int[] change) {
+        int total_usage = 0;
+        for(int i=0; i<change.length; i++){
+            usage = usage + usage * change[i] / 100;
+            total_usage += usage;
+            if(total_usage > storage){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
