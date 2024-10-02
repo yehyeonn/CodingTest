f0cfe52ead8fc920d70ded97b45740ea237f4a12
yehyeonn <yehyeon99@naver.com>
2024-10-02 22:40:21 +0900

    # Day62 백준 10171: 고양이

---
+++ src/Day62/_10171/Main.java
@@ -0,0 +1,10 @@
+package Day62._10171;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
