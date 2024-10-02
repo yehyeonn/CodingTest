3e46605b79fc6755427d021e16ced7090b7dcf49
yehyeonn <yehyeon99@naver.com>
2024-10-02 23:05:14 +0900

    # Day62 백준 10950: A+B - 3

---
+++ src/Day62/_10950/Main.java
@@ -0,0 +1,20 @@
+package Day62._10950;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i<= T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            System.out.println(A+B);
+        }
+    }
+}
