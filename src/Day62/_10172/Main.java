3662e78b94325fce9b415c35cc3a6f4cda61944e
yehyeonn <yehyeon99@naver.com>
2024-10-02 22:46:40 +0900

    # Day62 백준 10172: 개

---
+++ src/Day62/_10172/Main.java
@@ -0,0 +1,11 @@
+package Day62._10172;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+    }
+}
