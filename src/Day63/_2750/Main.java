20e2fc85e4f3851937f68181094a21696a5f2fd0
yehyeonn <yehyeon99@naver.com>
2024-10-03 20:11:32 +0900

    # Day63 백준 2750: 수 정렬하기

---
+++ src/Day63/_2750/Main.java
@@ -0,0 +1,22 @@
+package Day63._2750;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        for(int n : arr) {
+            System.out.println(n);
+        }
+    }
+}
