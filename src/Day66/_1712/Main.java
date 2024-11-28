3ee474e84dfddd46142cbfd0a2ac4a4f2653947a
yehyeonn <yehyeon99@naver.com>
2024-11-28 20:53:59 +0900

    # Day66 백준 1712: 손익분기점

---
+++ src/Day66/_1712/Main.java
@@ -0,0 +1,18 @@
+package Day66._1712;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        if(c <= b) System.out.print(-1);
+        else System.out.println(a/(c-b) + 1);
+    }
+}
