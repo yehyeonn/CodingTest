e50f0e0d0a330089584956e72614979a4eafab77
yehyeonn <yehyeon99@naver.com>
2024-12-05 14:08:37 +0900

    # Day70 프로그래머스: 예산

---
+++ src/Day70/programmers/Solution.java
@@ -0,0 +1,19 @@
+package Day70.programmers;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+
+        int sum = 0;
+        int answer = 0;
+
+        for(int i = 0; i < d.length; i++) {
+            sum += d[i];
+            if(sum <= budget) answer++;
+        }
+
+        return answer;
+    }
+}
