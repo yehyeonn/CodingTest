288023a68318d68520af796952e446e111de8b65
yehyeonn <yehyeon99@naver.com>
2024-12-11 22:57:47 +0900

    # Day73 LeetCode: Remove Duplicates from Sorted Array

--- src/Day73/LeetCode/_26/Solution.java
+++ src/Day73/LeetCode/_26/Solution.java
@@ -1,14 +1,14 @@
 package Day73.LeetCode._26;
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int n = 0;
+        int k = 0;
 
         for(int i = 1; i < nums.length; i++) {
-            if(nums[n] != nums[i]) {
-                n++;
-                nums[n] = nums[i];
+            if(nums[k] != nums[i]) {
+                k++;
+                nums[k] = nums[i];
             }
         }
-        return n+1;
+        return k+1;
     }
 }
