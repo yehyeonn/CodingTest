56801f7ca96074e9de4d4b5678aa9c458daaadfa
yehyeonn <yehyeon99@naver.com>
2024-12-17 17:00:08 +0900

    # Day76 LeetCode: Best Time to Buy and Sell Stock

---
+++ src/Day76/LeetCode/_121/Solution.java
@@ -0,0 +1,14 @@
+package Day76.LeetCode._121;
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+
+        for(int price: prices) {
+            if(price < min) min = price;
+            else if(price - min > max) max = price - min;
+        }
+        return max;
+    }
+}
